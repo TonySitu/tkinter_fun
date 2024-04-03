02f77dd2bb3d18cd0e18d25e997a9d57acaf1da6
Tony <137113805+TonySitu@users.noreply.github.com>
2024-04-02 22:07:10 -0400

    simple tkinter app

---
+++ src/simple_conversion_gui.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+
+
+def convert() -> None:
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(str("{:,.2f}".format(km_output)))
+
+
+# creates window
+main_frame = ttk.Window(themename="journal")
+main_frame.title("Demo")
+main_frame.geometry("300x150")
+
+title_label = ttk.Label(master=main_frame, text="Miles to kilometers", font="Calibre 24 bold")
+# places label in window
+title_label.pack()
+
+input_frame = ttk.Frame(master=main_frame)
+entry_int = tk.IntVar()
+entry_field = ttk.Entry(master=input_frame, textvariable=entry_int)
+button = ttk.Button(master=input_frame, text="Convert", command=convert)
+# pack entry field and button into input frame
+entry_field.pack(side="left", padx=10)
+button.pack(side="left")
+# put input frame into main frame
+input_frame.pack(pady=10)
+
+output_string = tk.StringVar()
+output_label = ttk.Label(master=main_frame,
+                         text="Output",
+                         font="Calibre 24",
+                         textvariable=output_string)
+output_label.pack(anchor="s", pady=5)
+
+# runs the window
+main_frame.mainloop()
